9491dacc5150f1e9b844a0260ead4c767535a858
Will <wmalos22@gmail.com>
2021-02-27 19:40:11 -0800

    created click event for saving

--- Assets/script.js
+++ Assets/script.js
@@ -1,3 +1,7 @@
 // Shows todays date
 var currentDate = moment().format('dddd, MMM Do YYYY');
 $("#currentDay").html(currentDate);
+
+$(document).ready(function () {
+    // creates click event for save buttons
+    $(".saveBtn").on("click", function () {
