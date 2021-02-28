5b6c7b06bdbf75e3f0e0c1fea0814eff3967c1b4
Will <wmalos22@gmail.com>
2021-02-27 19:47:42 -0800

    saves  textr to local storage

--- Assets/script.js
+++ Assets/script.js
@@ -5,3 +5,11 @@ $("#currentDay").html(currentDate);
 $(document).ready(function () {
     // creates click event for save buttons
     $(".saveBtn").on("click", function () {
+
+        var text = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
+
+        // saves text to the specific time to your local storage
+        localStorage.setItem(time, text);
+    })
+})
