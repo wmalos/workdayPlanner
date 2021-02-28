f8afece1d5914ffd95ac18585c35991ae8d0725a
Will <wmalos22@gmail.com>
2021-02-27 21:40:05 -0800

    debugging saving to local storage

--- Assets/script.js
+++ Assets/script.js
@@ -40,18 +40,19 @@ $(document).ready(function () {
 
             }
         })
-    
+    }
+
     // gets item from local storage
-    $("#hour8 .description").val(localStorage,getItem("hour8"));
-    $("#hour9 .description").val(localStorage,getItem("hour9"));
-    $("#hour10 .description").val(localStorage,getItem("hour10"));
-    $("#hour11 .description").val(localStorage,getItem("hour11"));
-    $("#hour12 .description").val(localStorage,getItem("hour12"));
-    $("#hour13 .description").val(localStorage,getItem("hour13"));
-    $("#hour14 .description").val(localStorage,getItem("hour14"));
-    $("#hour15 .description").val(localStorage,getItem("hour15"));
-    $("#hour16 .description").val(localStorage,getItem("hour16"));
-    $("#hour17 .description").val(localStorage,getItem("hour17"));
+    $("#hour8 .description").val(localStorage.getItem("hour8"));
+    $("#hour9 .description").val(localStorage.getItem("hour9"));
+    $("#hour10 .description").val(localStorage.getItem("hour10"));
+    $("#hour11 .description").val(localStorage.getItem("hour11"));
+    $("#hour12 .description").val(localStorage.getItem("hour12"));
+    $("#hour13 .description").val(localStorage.getItem("hour13"));
+    $("#hour14 .description").val(localStorage.getItem("hour14"));
+    $("#hour15 .description").val(localStorage.getItem("hour15"));
+    $("#hour16 .description").val(localStorage.getItem("hour16"));
+    $("#hour17 .description").val(localStorage.getItem("hour17"));
 
     // call function
     timeTrack();
